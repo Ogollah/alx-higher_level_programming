1197334076fb5bc625b0abc9d58dc68b4d928f66
Ogollah <kogollah2011@gmail.com>
2023-06-06 22:20:16 +0300

    print upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        num = 32 if ord(char) > 96 and ord(char) < 123 else 0
+        print("{:c}".format(ord(char) - num), end='')
+    print()
+ 
