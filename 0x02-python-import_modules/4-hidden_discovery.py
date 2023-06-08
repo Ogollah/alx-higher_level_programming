01b1c9e614567f0f2477bde3e154a7c39cbfc55a
Ogollah <kogollah2011@gmail.com>
2023-06-08 22:52:22 +0300

    printing hidden files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-import dis
-import types
 
 
-def print_defined_names(module):
-    names = []
-    for const in module.co_consts:
-        if isinstance(const, types.CodeType):
-            names.extend(const.co_names)
-    names = sorted(set(names))
-    
+if __name__ == "__main__":
+    import hidden_4
+
+    names = [name for name in dir(hidden_4) if not name.startswith('__')]
     for name in names:
-        if not name.startswith("__"):
-            print(name)
-
-
-module = __import__("hidden_4")
-
-
-print_defined_names(module)
+        print(name)
