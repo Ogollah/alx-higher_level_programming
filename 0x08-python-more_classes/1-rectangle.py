0e1310a36f3a9de6d8e95fca2bdae1d507f53bf2
Ogollah <kogollah2011@gmail.com>
2023-07-03 22:52:51 +0300

    rectangle class def lenth and width.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,9 +31,9 @@ class Rectangle:
         return self._height
 
     @height.setter
-    def heigh(self, value):
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self._height = value
