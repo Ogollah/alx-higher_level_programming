1380e9e6736f85a3da40255adc47455bfd2f88c4
Ogollah <kogollah2011@gmail.com>
2023-07-03 22:07:47 +0300

    rectangle class def lenth and width.

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+""" Class defining a Rectangle. """
+
+
+class Rectangle:
+    """Defining a rectangle class """
+
+    def __init__(self, width=0, height=0) -> None:
+        self._width = 0
+        self._height = 0
+        self._height = height
+        self._width = width
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def heigh(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._height = value
