9e2c2e98f9618accad9f32b2be9c68c454d22fd1
Ogollah <kogollah2011@gmail.com>
2023-07-04 22:50:58 +0300

    :construction: magic string and low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,8 +6,3 @@ class LockedClass:
     """ Prevets user not to instantiatte a new Locked Class"""
 
     __slots__ = ["first_name"]
-
-    def __setattr__(self, attr, value):
-        if attr != "first_name":
-            raise AttributeError("Cannot only set attribute 'first_name'")
-        super().__setattr__(attr, value)
