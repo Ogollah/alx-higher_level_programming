d7e16a277db4740df73418a87d521640c82863ac
Ogollah <kogollah2011@gmail.com>
2023-07-05 21:59:09 +0300

    fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python3
+
+""" Rectangle class. """
+
+
 class Rectangle:
-    """Defining a rectangle class."""
+    """Defining a rectangle class """
 
     def __init__(self, width=0, height=0):
-        """Initialize the width and the height of the rectangle."""
+        """ Initialize the width and the heigth of the rectangle. """
+
         self._width = 0
         self._height = 0
         self.width = width
@@ -33,21 +39,22 @@ class Rectangle:
         self._height = value
 
     def area(self):
-        """Calculate the area of the rectangle."""
+        """Area of a Rectangle"""
+
         return self._width * self._height
 
     def perimeter(self):
-        """Calculate the perimeter of the rectangle."""
+        """Perimeter"""
+
         if self._width == 0 or self._height == 0:
             return 0
         return 2 * (self._width + self._height)
 
     def __str__(self):
         """ String representation of the rectangle."""
-        if self._height == 0 or self._width == 0:
+
+        if self._width == 0 or self._height == 0:
             return ""
-        size = "#" * self._width
-        rect = []
-        for _ in range(self._height):
-            rect.append(size)
-        return "\n".join(rect)
+        rectangle_str = "#" * self._width + "\n"
+        rectangle_str *= self._height
+        return rectangle_str
