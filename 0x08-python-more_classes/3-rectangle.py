76c329cbf47bb131169429ca4546b691cc1ca0cd
Ogollah <kogollah2011@gmail.com>
2023-07-05 21:56:52 +0300

    fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,14 +1,8 @@
-#!/usr/bin/python3
-
-""" Rectangle class. """
-
-
 class Rectangle:
-    """Defining a rectangle class """
+    """Defining a rectangle class."""
 
     def __init__(self, width=0, height=0):
-        """ Initialize the width and the heigth of the rectangle. """
-
+        """Initialize the width and the height of the rectangle."""
         self._width = 0
         self._height = 0
         self.width = width
@@ -39,22 +33,21 @@ class Rectangle:
         self._height = value
 
     def area(self):
-        """Area of a Rectangle"""
-
+        """Calculate the area of the rectangle."""
         return self._width * self._height
 
     def perimeter(self):
-        """Perimeter"""
-
+        """Calculate the perimeter of the rectangle."""
         if self._width == 0 or self._height == 0:
             return 0
         return 2 * (self._width + self._height)
 
     def __str__(self):
         """ String representation of the rectangle."""
-
-        if self._width == 0 or self._height == 0:
+        if self._height == 0 or self._width == 0:
             return ""
-        rectangle_str = "#" * self._width + "\n"
-        rectangle_str *= self._height
-        return rectangle_str
+        size = "#" * self._width
+        rect = []
+        for _ in range(self._height):
+            rect.append(size)
+        return "\n".join(rect)
