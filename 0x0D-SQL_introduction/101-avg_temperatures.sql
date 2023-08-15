4e1e8bb0450545183872ab54b7b291b6f78ea213
Ogollah <kogollah2011@gmail.com>
2023-08-15 22:52:46 +0300

    Advanced

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- displays the average temperature
+SELECT city,
+    avg(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,7 @@
+-- Max temperatures in 3 states
+SELECT state,
+    MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state ASC
+LIMIT 3;
